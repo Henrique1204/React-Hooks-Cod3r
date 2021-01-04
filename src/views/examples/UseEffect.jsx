65f24fc8251fc47d8f5dc1f3c9b957eb5f21b626
Paulo <pauloh16.pdr@gmail.com>
2021-01-04 13:09:29 -0300

    Adicioner exercicio 1 sobre useEffect

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React from 'react';
+import PageTitle from '../../components/layout/PageTitle';
+import SectionTitle from '../../components/layout/SectionTitle';
+
+function calcularFat(n) {
+    const number = Number(n);
+
+    if (number < 0 || n === "") return -1;
+    if (number === 0) return 1;
+
+    return calcularFat(number - 1) * number;
+}
 
 const UseEffect = (props) => {
+    const [number, setNumber] = React.useState(1);
+    const [fatorial, setFatorial] = React.useState(1);
+
+    React.useEffect(() => {
+        console.log(Number(number));
+        setFatorial(calcularFat(number));
+    }, [number]);
+
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
-        </div>
-    )
-}
 
-export default UseEffect
+            <SectionTitle title="Exercício #01" />
+            <div className="center">
+                <p className="text">
+                    Fatorial: <span className="red">{fatorial === -1 ? "Valor não existe" : fatorial}</span>
+                </p>
+                <input className="input" type="number" value={number} onChange={({ target }) => setNumber(target.value)} />
+            </div>
+        </div>
+    );
+};
+
+export default UseEffect;
