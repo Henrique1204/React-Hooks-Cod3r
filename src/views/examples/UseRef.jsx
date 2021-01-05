8c2251195a247a8f75a786a21193551adf011234
Paulo <pauloh16.pdr@gmail.com>
2021-01-05 07:28:48 -0300

    Adicionar exercicio 1 sobre useRef

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React from 'react';
+import PageTitle from '../../components/layout/PageTitle';
+import SectionTitle from '../../components/layout/SectionTitle.jsx';
 
 const UseRef = (props) => {
+    const [value1, setValue1] = React.useState("");
+    const count = React.useRef(0);
+
+    React.useEffect(() => {
+        count.current = count.current + 1;
+    }, [value1]);
+
     return (
         <div className="UseRef">
             <PageTitle
                 title="Hook UseRef"
                 subtitle="Retorna um objeto mutável com a propriedade .current!"
             />
-        </div>
-    )
-}
 
-export default UseRef
+            <SectionTitle title="Exercício #01" />
+            <div className="center">
+                <p className="text">Valor: {value1} [<span className="red">{count.current}</span>]</p>
+                <input type="text"className="input" value={value1} onChange={({ target }) => setValue1(target.value)} />
+            </div>
+        </div>
+    );
+};
+
+export default UseRef;
