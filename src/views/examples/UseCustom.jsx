6ee5d257d79c601219f541bcec2591f10c220f71
Paulo <pauloh16.pdr@gmail.com>
2021-01-07 06:14:13 -0300

    Adicionar exercicio 1 sobre Custom Hooks

--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React from 'react';
+import PageTitle from '../../components/layout/PageTitle';
+import SectionTitle from '../../components/layout/SectionTitle.jsx';
+import useCounter from '../../Hooks/useCount.js';
 
-const UseRef = (props) => {
+const UseCustom = (props) => {
+    const [count, inc, dec] = useCounter(0);
+
     return (
         <div className="UseCustom">
             <PageTitle
                 title="Seu Hook"
                 subtitle="Vamos aprender como criar o nosso próprio Hook!"
             />
-        </div>
-    )
-}
 
-export default UseRef
+            <SectionTitle title="Exercício #01" />
+            <div className="center">
+                <span className="text">{count}</span>
+                <div>
+                    <button className="btn" onClick={inc} >+</button>
+                    <button className="btn" onClick={dec} >-</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default UseCustom;
